9e4e781fb5453b056a488aac708c1100a862a62b
Medet Kurganbayev <190103471@stu.sdu.edu.kz>
2021-02-21 21:17:43 +0600

    lab3_3 1commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('asset/CSSFolder/user.css') }}">
+	<script type="text/javascript" src="{{ asset('asset/JSFolder/user.css') }}"></script>
+</head>
+<body>
+	<h1 onclick="click()">I am a i=user blade.php</h1>
+	<a href="{{route('welcome')}}">welcome</a>
+
+</body>
+</html>
